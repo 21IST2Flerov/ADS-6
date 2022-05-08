2ae89ceeb0fd538008be8ed78fbe55c3dbcbbebd
21IST2Flerov <99124529+21IST2Flerov@users.noreply.github.com>
2022-05-08 21:58:50 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,42 +1,40 @@
 // Copyright 2022 NNTU-CS
-
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 template<typename T, int size>
 class TPQueue {
-private:
+ private:
     T arr[100];
-    int pervoe;
-    int poslednee;
+    int first;
+    int last;
 
-public:
-    TPQueue() : pervoe(0), poslednee(0) {}
+ public:
+    TPQueue() : first(0) , last(0) {}
     void push(T z1) {
-        if (poslednee - pervoe >= size) {
-            throw "Full!";
-        } else {
-            int z2 = poslednee - 1;
-            while ((z2 >= pervoe) && (arr[z2 % size].prior < z1.prior)) {
-                arr[(z2 + 1) % size] = arr[z2 % size];
-                z2--;
-            }
-            arr[(z2 + 1) % size] = z1;
-            poslednee++;
+      if (last - first >= size) {
+        throw "Full!";
+      } else {
+        int z2 = last - 1;
+        while ((z2 >= first) && (arr[z2 % size].prior < z1.prior)) {
+          arr[(z2 + 1) % size] = arr[z2 % size];
+          z2--;
         }
+        arr[(z2 + 1) % size] = z1;
+        last++;
+      }
     }
     T front() {
-        return arr[pervoe % size];
+      return arr[first % size];
     }
     T back() {
-        return arr[(poslednee - 1) % size];
+      return arr[(last - 1) % size];
     }
     T pop() {
-        return arr[(pervoe++) % size];
+      return arr[(first++) % size];
     }
 };
 struct SYM {
-    char ch;
-    int prior;
+  char ch;
+  int prior;
 };
-
 #endif  // INCLUDE_TPQUEUE_H_
